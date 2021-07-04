35e04990b85b7b9673df5858ed718282f10ba6fd
Dima Glushkov <dglushkov@mail.ru>
2021-07-05 03:41:38 +0700

    final

---
+++ _nuxt/static/1625431260/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Foblaka-studio\u002F_nuxt\u002Fstatic\u002F1625431260",layout:"default",error:a,state:{modalContent:a,isModalOpen:false},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Foblaka-studio\u002F",assetsPath:"\u002Foblaka-studio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4e235ba3"}}}}(null));
