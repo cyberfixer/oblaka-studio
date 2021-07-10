84e1f622c163ebe8ea3494baa9ed928ded835742
Dima Glushkov <dglushkov@mail.ru>
2021-07-11 06:36:43 +0700

    final commit gh-pages deploy

--- _nuxt/static/1625959780/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625959780",layout:"default",error:a,state:{modalContent:a,isModalOpen:false},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"807fc9bb"}}}}(null,"\u002F"));
